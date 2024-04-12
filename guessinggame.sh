15bc01764ed9a4b86808a8a9a94dd38c04511255
hamzatx <hamzatabat4@gmail.com>
2024-04-12 21:00:53 +0000

    updated guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-numfil= $(ls | wc -l)
+numfil=$(ls | wc -l)
 
 function guessfilnum {
+while true; do
 	read g
 	if [[ $g -eq $numfil ]]
 	then
 	 echo " your guess is correct "
 	else
-	  if [[ $g -gt $numfil ]]
-	  then
+	if  [[ $g -gt $numfil ]]
+	then
 	    echo " too high... " 
 	    guessfilnum
-	  else
+	else
 	    echo " too low... " 
             guessfilnum
-	  fi
 	fi
-
+fi
+break
+done
 }
 echo " guess how many files are in the  directory:  "
 guessfilnum
